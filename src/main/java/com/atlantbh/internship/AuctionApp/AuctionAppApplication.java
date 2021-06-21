95b7b6f11056819988b8bf4cb0c6dd0decfacfdd
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2021-06-21 19:54:28 +0200

    Initial commit

---
+++ src/main/java/com/atlantbh/internship/AuctionApp/AuctionAppApplication.java
@@ -0,0 +1,13 @@
+package com.atlantbh.internship.AuctionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuctionAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atlantbh/internship/AuctionApp/AuctionAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atlantbh.internship.AuctionApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuctionAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
