db5bde374a886a6babb9312f42c81a4a858fc7b9
Ajla Dzaferbegovic <56268385+adzaferbeg1@users.noreply.github.com>
2021-07-08 15:15:29 +0200

    Implement email verification logic

--- backend/src/main/java/com/atlantbh/internship/AuctionApp/service/EmailService.java
+++ backend/src/main/java/com/atlantbh/internship/AuctionApp/service/EmailService.java
@@ -1,4 +1,40 @@
 package com.atlantbh.internship.AuctionApp.service;
 
-public class EmailService {
+import com.atlantbh.internship.AuctionApp.model.EmailSender;
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements EmailSender {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    @Async
+    public void send(String to, String email) {
+        try{
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your e-mail address");
+            helper.setFrom("not-useable@auction.com");
+            javaMailSender.send(mimeMessage);
+
+        }catch (MessagingException e){
+            LOGGER.error("Failed to send email", e);
+            throw new IllegalStateException("Failed to send");
+        }
+
+    }
 }
